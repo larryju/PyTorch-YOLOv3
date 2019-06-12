96b5b94b7995f0886d209c1372eab7610cff4a62
jky <jukangyong@eastedu.com>
2019-06-12 13:05:27 +0800

    修改生成train.txt逻辑

--- utils/face_yolo/build_txt.py
+++ utils/face_yolo/build_txt.py
@@ -3,17 +3,23 @@
 """
 from __future__ import division
 import os
+import argparse
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_path", type=str, default="E:\\datasets\\face\\WIDER_val\\images", help="root of image")
+    parser.add_argument("--out_name", type=str, default='train.txt', help="images txt")
 
-image_root = "E:\\datasets\\face\\WIDER_val\\images"
+    opt = parser.parse_args()
 
-train_txt = open('valid.txt', encoding='utf-8', mode='a+')
+    out_name = opt.out_name
+    image_root = opt.image_path
 
-prefix = image_root
+    train_txt = open(out_name, encoding='utf-8', mode='a+')
 
-for child_path in os.listdir(image_root):
-    for img in os.listdir(os.path.join(image_root, child_path)):
-        line = os.path.join(image_root, child_path, img)
-        train_txt.write(line + '\n')
+    for child_path in os.listdir(image_root):
+        for img in os.listdir(os.path.join(image_root, child_path)):
+            line = os.path.join(image_root, child_path, img)
+            train_txt.write(line + '\n')
 
-train_txt.close()
+    train_txt.close()
